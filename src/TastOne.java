5f3b7270390434abe8587240fa79e254f470f0a4
SabaMicheal <57517096+SabaMicheal@users.noreply.github.com>
2019-11-10 12:29:19 -0600

    Test

---
+++ src/TastOne.java
@@ -0,0 +1,9 @@
+public class TastOne {
+    public static void main(String[]args){
+        System.out.println("This is my Test");
+        System.out.println("This is my Test 1");
+        System.out.println("This is my Test 2");
+        System.out.println("This is my Test 3");
+        System.out.println("This is my Test 4");
+    }
+}
